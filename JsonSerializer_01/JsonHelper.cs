2845dedc2d135342b3f8cd92e3c52a8bee57ac5f
bkrauska_ft <bkrauska@fellowshiptech.com>
2011-01-13 16:37:11 -0600

    Initial commit

---
+++ JsonSerializer_01/JsonHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace JsonSerializer_01 {
+    public class JsonHelper {
+        public static string Serialize<T>(T obj) {
+            var serializer = new DataContractJsonSerializer(obj.GetType());
+            var ms = new MemoryStream();
+            serializer.WriteObject(ms, obj);
+            var retVal = Encoding.Default.GetString(ms.ToArray());
+            ms.Dispose();
+            return retVal;
+        }
+
+        public static T Deserialize<T>(string json) {
+            T obj = Activator.CreateInstance<T>();
+            var ms = new MemoryStream(Encoding.Unicode.GetBytes(json));
+            var serializer = new DataContractJsonSerializer(obj.GetType());
+            obj = (T)serializer.ReadObject(ms);
+            ms.Close();
+            ms.Dispose();
+            return obj;
+        }
+    }
+}
